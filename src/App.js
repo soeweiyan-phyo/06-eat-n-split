80ede77c9745e4c82a771198f2966697a51060ab
Soe Wei Yan Phyo <38011380+soeweiyan-phyo@users.noreply.github.com>
2023-08-10 05:03:33 +1000

    Reset split form with key prop

--- src/App.js
+++ src/App.js
@@ -79,6 +79,7 @@ function App() {
 
       {selectedFriend && (
         <FormSplitBill
+          key={selectedFriend.id}
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
         />
